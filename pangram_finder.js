aaffb1a458ea1d605289794611cb4ff2071d069a
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-08-31 22:30:39 +0100

    Finished pangram

--- pangram_finder.js
+++ pangram_finder.js
@@ -1,9 +1,14 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase.toLowerCase().split("")
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((alphaChar) => {
+    return alphaChar === this.phrase.find((charToFind)=>{
+        return charToFind === alphaChar
+    }); 
+  });
 }
 
 module.exports = PangramFinder;
